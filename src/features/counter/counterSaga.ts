edc9dd29b160523eb0a6f66dcbfbc5063a894bfa
Viet Cuong <hosyvietcuong@gmail.com>
2021-09-10 17:01:43 +0700

    Watch log from middleware

--- src/features/counter/counterSaga.ts
+++ src/features/counter/counterSaga.ts
@@ -1,3 +1,12 @@
+import {all, takeEvery} from "@redux-saga/core/effects"
+import { PayloadAction } from "@reduxjs/toolkit";
+import { increment } from "./counterSlice";
+
+export function* log(action: PayloadAction) {
+    console.log('Log', action)
+}
+
 export default function* counterSaga() {
-    
+    console.log('counter saga')
+    yield takeEvery(increment().type, log)
 }
